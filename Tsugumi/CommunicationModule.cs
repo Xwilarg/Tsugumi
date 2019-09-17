ea7c10eb890fb8e19d1331774fccc3776a8cb5c3
Xwilarg <xwilarg@yahoo.fr>
2019-09-17 23:02:50 +0900

    Add basic Discord bot

---
+++ Tsugumi/CommunicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using DiscordUtils;
+using System.Threading.Tasks;
+
+namespace Tsugumi
+{
+    public class CommunicationModule : ModuleBase
+    {
+        [Command("Info")]
+        public async Task Info()
+        {
+            await ReplyAsync("", false, Utils.GetBotInfo(Program.P.StartTime, "Tsugumi", Program.P.client.CurrentUser));
+        }
+    }
+}
